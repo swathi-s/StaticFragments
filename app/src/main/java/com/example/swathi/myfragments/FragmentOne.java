20c7c24b0a338e14600ff35dd3f10bf9599600bc
swathi-s <swathi.ehr@gmail.com>
2016-10-12 16:58:24 +0530

    Simple application to show how to add 2 different static fragments in android

---
+++ app/src/main/java/com/example/swathi/myfragments/FragmentOne.java
@@ -0,0 +1,39 @@
+package com.example.swathi.myfragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by user on 10/12/2016.
+ */
+
+public class FragmentOne extends Fragment {
+
+    ListView listView;
+
+    ArrayAdapter<String> adapter;
+
+    String[] versions;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_one_layout,container,false);
+
+        listView = (ListView) view.findViewById(R.id.list_view);
+
+        versions = getResources().getStringArray(R.array.android_versions);
+
+        adapter = new ArrayAdapter<String>(getActivity(),R.layout.list_view_layout,R.id.row_item,versions);
+
+        listView.setAdapter(adapter);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/swathi/myfragments/FragmentTwo.java
@@ -0,0 +1,21 @@
+package com.example.swathi.myfragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by user on 10/12/2016.
+ */
+
+public class FragmentTwo extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_two_layout, container, false);
+        return view;
+    }
+}
